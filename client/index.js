41aee688777198f10b96cf1d83829cd2f0eccc52
stefkwan <stefanikwan@gmail.com>
2019-06-16 15:58:15 +0700

    edit form ok

--- client/index.js
+++ client/index.js
@@ -113,6 +113,7 @@ function hideAddTodoForm(){
 //------------ populate todo list ----------
 
 function populateTodo(){
+  $('#theTodoList').empty()
   $('#theTodoList').append("Loading todo list...")
   $.ajax({
     method: "GET",
@@ -132,6 +133,124 @@ function populateTodo(){
   })
 }
 
+// -------------- appending todo ---------
+function getDateInFormat(date){
+  let yyyy = new Date(date).getFullYear()
+  let mm = new Date(date).getMonth()+1
+  if (mm<10) mm = '0'+mm
+  let dd = new Date(date).getDate()
+  if (dd<10) dd = '0'+dd
+  let dateYYYYMMDD = yyyy+'-'+mm+'-'+dd
+  return dateYYYYMMDD
+}
+
+function appendTodoEditForm(todo){
+  let htmlTodoEditForm = `
+  <div id="editTodoForm${todo._id}" class="editTodoForm">
+    <form id="editTodo${todo._id}" class="form1" action="/" method="PATCH" autocomplete="off">
+      <div class="form1title">Edit a Todo</div>
+      <input class="input1" type="text" value="${todo.name}" name="name">
+      <br>
+      <input class="input1" type="text" value="${todo.description}" name="description">
+      <br>`
+
+  let htmlStatusRadioButtons = `<label>Status:</label><br>`
+  if(todo.status == "done"){
+    htmlStatusRadioButtons +=
+    `<input class="" type="radio" name="status" value="not done"> Not done<br>
+      <input class="" type="radio" name="status" value="in progress"> In progress<br>
+      <input class="" type="radio" name="status" value="done" checked> Done<br>`
+
+  } else if (todo.status == "in progress"){
+    htmlStatusRadioButtons +=
+    `<input class="" type="radio" name="status" value="not done"> Not done<br>
+      <input class="" type="radio" name="status" value="in progress" checked> In progress<br>
+      <input class="" type="radio" name="status" value="done"> Done<br>`
+
+  } else { //todo.status == 'not done'
+    htmlStatusRadioButtons +=
+    `<input class="" type="radio" name="status" value="not done" checked> Not done<br>
+      <input class="" type="radio" name="status" value="in progress"> In progress<br>
+      <input class="" type="radio" name="status" value="done"> Done<br>`
+
+  }
+      
+  htmlTodoEditForm += htmlStatusRadioButtons
+
+  let dateYYYYMMDD = getDateInFormat(todo.dueDate)
+  
+  htmlTodoEditForm +=
+  `<input class="input1" type="Date" value="${dateYYYYMMDD}" name="dueDate">
+      <br>
+      <a href="#" onclick="hideEditTodoForm('${todo._id}')">cancel edit</a>|
+      <a href="#" onclick="editTodo('${todo.owner},${todo._id}')">update todo</a>
+    </form>
+  </div>`
+  $('#theTodoList').append(htmlTodoEditForm)
+}
+
+function appendTodo(todo){
+  let htmlTodo = `
+  <div id="nonEditTodoDiv${todo._id}">
+    <ul>
+      <li>Name: ${todo.name}</li>
+      <li>Description: ${todo.description}</li>
+      <li>Status: ${todo.status}</li>
+      <li>Due Date: ${new Date(todo.dueDate).toDateString()}</li>
+      <li><a href="#" onclick="showEditTodoForm('${todo._id}')">edit</a>|
+      <a href="#" onclick="delTodo('${todo.owner},${todo._id}')">delete</a></li>
+    </ul>
+  </div>
+`
+  $('#theTodoList').append(htmlTodo)
+  appendTodoEditForm(todo)
+  $('.editTodoForm').hide()
+}
+
+// ---------- editing a todo -------------
+function showEditTodoForm(todoId){
+  $(`#nonEditTodoDiv${todoId}`).hide()
+  $(`#editTodoForm${todoId}`).show()
+}
+
+function hideEditTodoForm(todoId){
+  $(`#editTodoForm${todoId}`).hide()
+  $(`#nonEditTodoDiv${todoId}`).show()
+}
+
+function editTodo(strInput){
+  let [userId, todoId] = strInput.split(',')
+
+  let name = $(`#editTodo${todoId} input[name='name']`).val()
+  let description = $(`#editTodo${todoId} input[name='description']`).val()
+  let status = $(`#editTodo${todoId} input[name='status']:checked`).val()
+  let dueDate = $(`#editTodo${todoId} input[name='dueDate']`).val()
+  // console.log("editform input values:")
+  // console.log(name,description,status,dueDate)
+
+  $.ajax({
+      method: "PATCH",
+      url: `${baseUrl}/todos/update/${userId}/${todoId}`,
+      headers: {
+        access_token: localStorage.getItem('access_token')
+      },
+      data: {
+        name: name,
+        description: description,
+        status: status,
+        dueDate: dueDate
+      }
+    })
+    .done(result => {
+      console.log("updated a todo", result)
+      populateTodo()
+    })
+    .fail(err => {
+      console.log(err)
+    })
+
+}
+
 // --------- deleting a todo -----------
 function delTodo(strInput){
   let [userId, todoId] = strInput.split(',')
@@ -144,23 +263,14 @@ function delTodo(strInput){
     })
     .done(result => {
       console.log("deleted a todo", result)
+      populateTodo()
     })
     .fail(err => {
       console.log(err)
     })
 }
 
-// -------------- appending todo ---------
-function appendTodo(todo){
-  let htmlTodo = `<ul>
-    <li>Name: ${todo.name}</li>
-    <li>Description: ${todo.description}</li>
-    <li>Status: ${todo.status}</li>
-    <li>Due Date: ${new Date(todo.dueDate).toDateString()}</li>
-    <li><button type="submit" onclick="delTodo('${todo.owner},${todo._id}')">delete</button></li>
-  </ul>`
-  $('#theTodoList').append(htmlTodo)
-}
+//-----------------------------------------------------
 
 $(document).ready(() => {
   // api/todos/add
