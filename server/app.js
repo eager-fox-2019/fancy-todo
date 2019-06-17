c065dcda46e96880e9ef16509d5c017615c555e4
dedysimamora <dedysimamora627@gmail.com>
2019-06-17 07:45:43 +0700

    add documentation

---
+++ server/app.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+var cors = require('cors')
+const port = process.env.PORT || 3100
+const CONNECTION_URI = process.env.MONGGODB_URI || `${process.env.MONGGODB_URL}/${process.env.MONGGODB_NAME}`
+const routes = require('./routes/')
+var mongoose = require('mongoose');
+mongoose.connect(CONNECTION_URI, {useNewUrlParser: true})
+.then(() =>{
+      console.log('MongoDB connected')
+})
+.catch(err =>{
+      console.log(err)
+})
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(cors())
+app.use('/api', routes)
+app.use((err,req,res,next) => {
+    if(err.code === 404){
+        res.status(404).json({
+            message : "Not Found"
+        })
+    } else if(err.name === 'ValidationError'){
+        var errorArr = []
+        for (let listErr in err.errors) {
+            errorArr.push({
+                message : err.errors[listErr].message,
+                path : err.errors[listErr].path
+            })
+        }
+        res.status(400).json({
+            errorArr
+        })
+    } else if(err.code === 500) {
+        res.status(500).json({
+            message : 'Internal Server Error'
+        })
+    } else {
+        res.status(err.code).json({
+            message : err.message
+        })
+    }
+})
+app.listen(port, () => {
+    console.log('this app running on port ' + port)
+})
