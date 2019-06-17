2a4c1e4f0870ef6d281b7b1da9988ecc08883af7
muhamadnabila <mbilalghz.gmail.com>
2019-06-17 21:52:17 +0700

    delete console.log on client

--- client/script/error.js
+++ client/script/error.js
@@ -1,12 +1,21 @@
 function showError(err) {
-    console.log(err.response)
-    $('body').html(
-        `
-        <div class="container d-flex flex-column mt-5" style="border:1px solid red;">
-        <h1 style="color:yellow">${err.response.statusText}</h1>
-        <h1 style="color:red">${err.response.status}</h1>
-        <h1 style="color:red">${err.response.data.message}</h1>
-        </div>
-        `
-    )
+    if(err.response.status == 400) {
+        Swal.fire({
+            type: `${err.response.status}`,
+            title: `${err.response.statusText}`,
+            text: `${err.response.data.message}`,
+            footer: '<a href>Why do I have this issue?</a>'
+          })
+          initial()          
+    }else {
+        $('body').html(
+            `
+            <div class="container d-flex flex-column mt-5" style="border:1px solid red;">
+            <h1 style="color:yellow">${err.response.statusText}</h1>
+            <h1 style="color:red">${err.response.status}</h1>
+            <h1 style="color:red">${err.response.data.message}</h1>
+            </div>
+            `
+        )
+    }
 }
